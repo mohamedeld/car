f59c295f3520d640a1103ab721f19753b8dd069d
Mohamed Elrfaay <56503517+mohamedeld@users.noreply.github.com>
2020-11-23 21:38:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu').addEventListener('click',()=>{
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle("change");
+    });  
+});
+
+document.querySelectorAll('.wrapper').forEach((item)=>{
+    item.addEventListener('click',()=>{
+        document.querySelectorAll('.target').forEach((item)=>{
+            this.classList.remove("change");
+        })
+       
+    })
+})
